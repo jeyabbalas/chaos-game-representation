68c783c39a125e72ecd1525c95e378ce6a6b6912
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2022-03-11 01:13:35 -0500

    Implemented CGR forward calculations

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
+use enum_iterator::IntoEnumIterator;
 use flate2::read::GzDecoder;
 use rand::prelude::ThreadRng;
 use rand::{
@@ -12,19 +13,6 @@ use rand::{
     thread_rng, 
 };
 
-
-pub fn convert_decimal_to_binary(decimal: u32) -> [f64; 2] {
-    const RADIX: u32 = 2;
-    const WIDTH: usize = 2;
-
-    format!("{decimal:0WIDTH$b}")
-            .chars()
-            .flat_map(|c| c.to_digit(RADIX))
-            .map(|n| n as f64)
-            .collect::<Vec<f64>>()
-            .try_into()
-            .expect("Array wrong size compared to the iterator.")
-}
 
 #[allow(dead_code)]
 pub fn base_counts_in_fasta(file: File) -> HashMap<char, u32> {
@@ -45,12 +33,43 @@ pub fn base_counts_in_fasta(file: File) -> HashMap<char, u32> {
 }
 
 
-#[derive(Debug, Hash, Copy, Clone, Eq, PartialEq)]
+pub struct CGR {
+    forward: Vec<[f64;2]>,
+    backward: Vec<[f64;2]>,
+}
+
+
+#[derive(Debug, Hash, Copy, Clone, IntoEnumIterator, PartialEq, Eq)]
 enum Nucleotide {
     A, 
     C, 
     G, 
     T, 
+}
+
+
+pub fn convert_decimal_to_binary(decimal: usize) -> [f64; 2] {
+    const RADIX: u32 = 2;
+    const WIDTH: usize = 2;
+
+    format!("{decimal:0WIDTH$b}")
+            .chars()
+            .flat_map(|c| c.to_digit(RADIX))
+            .map(|n| n as f64)
+            .collect::<Vec<f64>>()
+            .try_into()
+            .expect("Array wrong size compared to the iterator.")
+}
+
+
+fn compute_cgr_edges() -> HashMap<Nucleotide, [f64; 2]> {
+    let mut cgr_edges: HashMap<Nucleotide, [f64; 2]> = HashMap::new();
+
+    for (idx, base) in Nucleotide::into_enum_iter().enumerate() {
+        cgr_edges.insert(base, convert_decimal_to_binary(idx));
+    }
+
+    cgr_edges
 }
 
 
@@ -82,29 +101,58 @@ fn str_to_nucleotides(s: &str) -> Vec<Nucleotide> {
     }).collect()
 }
 
-pub fn temp() -> () {
-    println!("{:?}", str_to_nucleotides("nnnnatgccATTN"));
+
+fn add(a: [f64; 2], b: [f64; 2]) -> [f64; 2] {
+    let mut c: [f64; 2] = [0.0; 2];
+    for(i, (a_val, b_val)) in a.iter().zip(&b).enumerate() {
+        c[i] = a_val + b_val;
+    }
+    c
 }
 
 
-pub struct CGR {
-    forward: Vec<f64>,
-    backward: Vec<f64>,
+fn subtract(a: [f64; 2], b: [f64; 2]) -> [f64; 2] {
+    let mut c: [f64; 2] = [0.0; 2];
+    for(i, (a_val, b_val)) in a.iter().zip(&b).enumerate() {
+        c[i] = a_val - b_val;
+    }
+    c
+}
+
+
+fn divide(a: [f64; 2], b: f64) -> [f64; 2] {
+    let mut c: [f64; 2] = [0.0; 2];
+    for(i, a_val) in a.iter().enumerate() {
+        c[i] = a_val/b;
+    }
+    c
 }
 
 
 pub fn compute_chaos_game_representation_from_fasta(file: File) -> CGR {
-    let cgr_edges: HashMap<Nucleotide, [f64; 2]> = HashMap::from([
-        (Nucleotide::A, [0.0, 1.0]),
-    ]);
+    let cgr_edges: HashMap<Nucleotide, [f64; 2]> = compute_cgr_edges();
     let reader: BufReader<GzDecoder<File>> = BufReader::new(GzDecoder::new(file));
 
-    let mut forward: Vec<f64> = Vec::new();
-    let mut backward: Vec<f64> = Vec::new();
+    let mut forward: Vec<[f64;2]> = Vec::new();
+    let mut backward: Vec<[f64;2]> = Vec::new();
+
+    let mut prev_point: [f64; 2] = [0.5; 2];
 
     for line in reader.lines().map(|l| l.unwrap()) {
-        println!("{}", line);
+        if (&line).starts_with(">") {
+            continue;
+        }
+        
+        let sequence = str_to_nucleotides(&line);
+        for base in sequence {
+            prev_point = add(prev_point, 
+                          divide(subtract(cgr_edges[&base], prev_point), 
+                                 2.0));
+            forward.push(prev_point);
+        }
     }
+
+    println!("{}", forward.len());
     
     CGR { forward, backward }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use std::path::Path;
 fn main() {
     let fasta_filepath: &Path = Path::new("./data/egfr/egfr.fa.gz");
     //let fasta_filepath: &Path = Path::new("./data/grch37/hg19.fa.gz");
-    //chaos_game_representation::parse_fasta_file(fasta_filepath);
-    chaos_game_representation::temp();
+    chaos_game_representation::parse_fasta_file(fasta_filepath);
+    //chaos_game_representation::temp();
 }
